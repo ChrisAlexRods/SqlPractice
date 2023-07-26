86eb5e92850b713ddad1e9eb2616642ff07e6580
Christian Rodriguez <chrisalexrods@gmail.com>
2023-07-26 12:07:41 -0400

    update specific value

---
+++ Codeacademy/updateValueinTable.sql
@@ -0,0 +1,9 @@
+UPDATE celebs
+set twitter_handle = "@taylorswift13"
+WHERE id = 4; 
+
+SELECT * FROM celebs;
+-- 1. UPDATE is a clause that edits a row in the table.
+-- 2. celebs is the name of the table.
+-- 3. SET is a clause that indicates the column to edit.
+-- 4. Where id is used to specify the row. 
