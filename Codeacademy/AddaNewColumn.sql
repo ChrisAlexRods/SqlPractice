bb4d9686b27e8431190fb14ce1ef53b4d688722f
Christian Rodriguez <chrisalexrods@gmail.com>
2023-07-26 11:56:45 -0400

    Add a new column

---
+++ Codeacademy/AddaNewColumn.sql
@@ -0,0 +1,12 @@
+ ALTER TABLE celebs 
+ ADD COLUMN 
+ twitter_handle TEXT; 
+
+ SELECT * FROM
+ celebs;
+
+--  The ALTER COMMAND is used to add columns into a table in
+-- conjunction with the table name. 
+
+-- To add the specific colummn use ADD COLUMN then the column name 
+-- name with the type. twitter_handle TEXT; 
