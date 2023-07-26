6cb3575cbab18e9df15f7921dd9a154bdf7c7b72
Christian Rodriguez <chrisalexrods@gmail.com>
2023-07-26 12:34:20 -0400

    Changing column name

---
+++ QueriesCodeAcademy/ChangeColumnName.sql
@@ -0,0 +1,10 @@
+SELECT name AS 
+Titles 
+FROM movies;
+
+SELECT imdb_rating  AS 
+IMDb 
+FROM movies;
+
+-- The AS keyword is used to rename a column name to something else.
+-- In this case changing name to titles and imdb_rating to IMDB
