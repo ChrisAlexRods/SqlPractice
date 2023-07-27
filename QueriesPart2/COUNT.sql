094e4ba609ca2ef5e41a80732213fa34d460cf17
Christian Rodriguez <chrisalexrods@gmail.com>
2023-07-27 12:55:43 -0400

    count

--- QueriesPart2/COUNT.sql
+++ QueriesPart2/COUNT.sql
@@ -1,4 +1,11 @@
 SELECT COUNT(*)
 from fake_apps;
 
-We wrap the asterik with COUNT() to return how many rows are in table. 
+-- We wrap the asterik with COUNT() to return how many rows are in table. 
+
+SELECT COUNT(*)
+FROM fake_apps
+WHERE price = 0;
+
+-- For this we are returning the number of free apps in the table. 
+-- The mistake i made during this was adding another ; 
