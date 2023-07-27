cdab11699fb146acb324edb308ffb94c1f903954
Christian Rodriguez <chrisalexrods@gmail.com>
2023-07-27 13:04:09 -0400

    min or max

---
+++ QueriesPart2/MaxandMin.sql
@@ -0,0 +1,12 @@
+SELECT MIN(downloads)
+FROM fake_apps;
+
+SELECT MAX(downloads)
+FROM fake_apps;
+
+-- Will show the highest and lowest downloaded apps. 
+
+SELECT MAX(price)
+FROM fake_apps;
+
+-- This one shows me the highest price app in the table. 
