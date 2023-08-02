7d71cb989d03f3fbe2a23faa93e32d742e0a439b
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-02 12:28:43 -0400

    jpn

---
+++ Hackerrank/jpn.sql
@@ -0,0 +1,6 @@
+SELECT * 
+FROM City
+WHERE COUNTRYCODE = "JPN";
+
+-- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- When you read the question ther sceond line is also telling you soemthing. 
