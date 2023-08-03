8d3668ca852b1607a86ce65b454e9e42d752388e
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-03 12:16:35 -0400

    6a

--- Hackerrank/6.sql
+++ Hackerrank/6.sql
@@ -8,3 +8,11 @@ WHERE CITY REGEXP '^[AEIOUaeiou]'
 -- For this question i used a new expression REGEXP. 
 -- ^ is used to represent the start of the string 
 -- the values in the [] are all the vowels.
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%' OR 
+       CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%')
+
+Here is the alternate answer. 
+the 'A%' is saying the first letter. For this we have to write all of them out. 
