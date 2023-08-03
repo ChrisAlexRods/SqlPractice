723446fa35ec0e8552da8fbd2c7c04053b8ba52d
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-03 12:44:40 -0400

    9a

--- EasySelect/9.sql
+++ EasySelect/9.sql
@@ -5,4 +5,11 @@ FROM STATION
 WHERE (CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%' AND 
        CITY NOT LIKE 'a%' AND CITY NOT LIKE 'e%' AND CITY NOT LIKE 'i%' AND CITY NOT LIKE 'o%' AND CITY NOT LIKE 'u%')
 
-Just like the previous query we now add NOT LIKE to signal i don't want this. 
+-- Just like the previous query we now add NOT LIKE to signal i don't want this. 
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT REGEXP '^[AEIOUaeiou]'
+
+-- This is the shorter simpler regex answer. 
+To find the last leter we put the $ at the end of the []
