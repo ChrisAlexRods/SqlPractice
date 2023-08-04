6056303a7de4872396c8c77d961deea7299f00c8
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-04 15:21:12 -0400

    16

---
+++ EasySelect/16.sql
@@ -0,0 +1,10 @@
+Write a query that prints a list of employee names (i.e.: the name attribute) 
+for employees in Employee having a salary greater than  per month who 
+have been employees for less than  months. Sort your result by ascending 
+employee_id.
+
+SELECT NAME 
+FROM Employee
+WHERE salary >2000 and months <10
+
+Made sure to select name and limit the other two
