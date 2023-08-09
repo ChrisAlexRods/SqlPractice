d6ec372c4b6e883d684349d5762ce188aaa3fdae
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-09 12:55:52 -0400

    2b

---
+++ Advanced Select/3PIVOT.sql
@@ -0,0 +1,11 @@
+SELECT 
+  MAX(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
+  MAX(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
+  MAX(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
+  MAX(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
+FROM (
+  SELECT Occupation, Name,
+         ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS Row
+  FROM OCCUPATIONS
+) SubQuery
+GROUP BY Row;
