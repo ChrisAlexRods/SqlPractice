d558d191c5a07aeb2f39ef4eeac60ef5254ee08e
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-10 11:18:20 -0400

    Final notes

--- Advanced Select/3PIVOT.sql
+++ Advanced Select/3PIVOT.sql
@@ -5,10 +5,10 @@ SELECT
   MAX(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
 FROM (
   SELECT Occupation, Name,
-         ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS Row
+         ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS RowNumber
   FROM OCCUPATIONS
 ) SubQuery
-GROUP BY Row;
+GROUP BY RowNumber;
 
 Each profession is being turned into its column 
 
@@ -23,4 +23,9 @@ CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END.
 This checks if the occupation is DOCTOR. if it is, return the name, 
 otherwise null 
 
-MAX(...) AS DOCTOR This takes the Maximum value 
+MAX(...) AS DOCTOR This takes the Maximum value for each row number, 
+, mkaing the first non null name for each row doctor. Its 
+auto making the row DOCTOR if it's not null. Same for the other profession. 
+
+Group By RowNumber groups the result by the row number assigned in 
+subquery. 
