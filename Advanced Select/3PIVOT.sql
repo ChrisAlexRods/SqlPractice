71235e886a8eb21bbace0688df23d3dc3e3a1e24
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-10 08:50:24 -0400

    notes

--- Advanced Select/3PIVOT.sql
+++ Advanced Select/3PIVOT.sql
@@ -9,3 +9,18 @@ FROM (
   FROM OCCUPATIONS
 ) SubQuery
 GROUP BY Row;
+
+Each profession is being turned into its column 
+
+The PARTITION BY is dividing the groups based on occupation 
+ORDER BY NAME orders the name alphabetically 
+ROW_NUMBER() OVER: Assigns a unique row nubmer to each name 
+
+MAX(CASE WHEN) AS Uses this expression to transform the occupation
+group into seperate columns
+
+CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END. 
+This checks if the occupation is DOCTOR. if it is, return the name, 
+otherwise null 
+
+MAX(...) AS DOCTOR This takes the Maximum value 
