0395a515a25811c7a5d2e5bd7261db53051eb46d
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-11 12:27:09 -0400

    175

--- sql/175.sql
+++ sql/175.sql
@@ -8,9 +8,11 @@ LEFT JOIN Address ON Person.personId = Address.personId;
 -- The statement should be broken down, becuase the FROM and join 
 -- statemnet are actually joining the two tables. 
 
-Left join is combining the Person and Adress tables based 
-on the personId column. 
+-- Left join is combining the Person and Adress tables based 
+-- on the personId column. 
 
-LEFT JOIN: returns all rows from the left table(PERSON) along
-with matching tables from the right table(Adress)
-    if there's no match the result is NULL for the right side
+-- LEFT JOIN: returns all rows from the left table(PERSON) along
+-- with matching tables from the right table(Adress)
+--     if there's no match the result is NULL for the right side
+--     We start with the Person.personId becasue that's what's being 
+--     left joined. 
