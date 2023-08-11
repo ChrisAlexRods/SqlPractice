db4b24a8c7aaeebe2fbdeb7086c0d6ee05ae324e
Christian Rodriguez <chrisalexrods@gmail.com>
2023-08-11 12:26:01 -0400

    MAking folder

---
+++ sql/175.sql
@@ -0,0 +1,16 @@
+SELECT firstName, lastName, city, state 
+FROM Person
+LEFT JOIN Address ON Person.personId = Address.personId;
+
+-- The select statement works becuase they are unique to each table. 
+-- So SQL Knows where each column is from. 
+
+-- The statement should be broken down, becuase the FROM and join 
+-- statemnet are actually joining the two tables. 
+
+Left join is combining the Person and Adress tables based 
+on the personId column. 
+
+LEFT JOIN: returns all rows from the left table(PERSON) along
+with matching tables from the right table(Adress)
+    if there's no match the result is NULL for the right side
